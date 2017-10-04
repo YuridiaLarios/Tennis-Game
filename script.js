d77c04cd5f43d279df330f96dc30629f11aa4ba6
Yuridia Larios Aispuro <yuriydani@gmail.com>
2017-10-04 01:49:09 -0700

    added computer paddle and boundaries

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ var ballSpeedX = 5;
 var ballY = 50;
 var ballSpeedY = 4;
 var paddle1Y = 250;
+var paddle2Y = 250;
 const PADDLE_HEIGHT = 100;
+const PADDLE_THICKNESS = 10;
 
 // event that fires everytime the mouse moves
 function calculateMousePos(evt){
   var rect = canvas.getBoundingClientRect(); // black rectangle area
   var root = document.documentElement;
   // taking position of mouse within playing space.
-  var mouseX = evt.clientX -rect.left -root.scrollLeft;
-  var mouseY = evt.clientY -rect.top - root.scrollTop;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top - root.scrollTop;
   // returning a pair of variables
   return {
     x:mouseX,
@@ -23,41 +25,76 @@ function calculateMousePos(evt){
 
 
 window.onload = function() { // as soon as page loads run this code
-  console.log("Hello World");
   canvas = document.getElementById('gameCanvas');
-  canvasContext = canvas.getContext('2d');
+  canvasContext = canvas.getContext('2d'); // necessary to draw on canvas
 
   var framesPerSecond = 30;
   setInterval(function(){
       moveEverything();
       drawEverything();
   }, 1000/framesPerSecond); // use to calculate our frame rates.
+
+// event listener to detect mouse movement and assigning y position to left paddle
+  canvas.addEventListener('mousemove',
+    function(evt){
+      var mousePos = calculateMousePos(evt);
+      paddle1Y = mousePos.y - PADDLE_HEIGHT/2;
+  });
+
 }
 
-/*
-function callBoth(){
-  moveEverything();
-  drawEverything();
+
+function ballReset(){
+  ballX = canvas.width/2;
+  ballY = canvas.height/2;
 }
-*/
+
+
+function computerMovement(){
+  var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
+  if(paddle2YCenter < ballY - 35){
+    paddle2Y += 5;
+  } else if(paddle2YCenter < ballY + 35){
+    paddle2Y -= 5;
+  }
+}
 
 function moveEverything(){
+  computerMovement();
+
   ballX = ballX + ballSpeedX;
   ballY = ballY + ballSpeedY;
 
-  if(ballY >= canvas.height || ballY <= 2){
-    ballSpeedY = -ballSpeedX; // flipping vertical direction to bounce inside canvas
+  if(ballX < 0){
+    if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT){
+      ballSpeedX = -ballSpeedX;
+    } else {
+      ballReset();
+    }
   }
-  if(ballX >= canvas.width || ballX <= 2){
-    ballSpeedX = -ballSpeedX; // flipping horizontal direction to bounce inside canvas
+  if(ballY >= canvas.height || ballY <= 0){
+    ballSpeedY = -ballSpeedY; // flipping vertical direction to bounce inside canvas
   }
+
+  if(ballX > canvas.width) {
+    if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
+      ballSpeedX = -ballSpeedX;
+    } else {
+      ballReset();
+    }
+  }
+
 }
 
 function drawEverything(){
   // gives the canvas screen a black color
   colorRect(0,0, canvas.width, canvas.height, 'black');
   // left player paddle
-  colorRect(10,210,10,PADDLE_HEIGHT,'white');
+  colorRect(2,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
+
+  // right computer paddle
+  colorRect(canvas.width -12,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
+
   // draws a circular ball
   colorCircle(ballX,ballY,10,'red');
 
@@ -74,5 +111,5 @@ function colorCircle(centerX, centerY, radius, drawColor){
 // helper function to draw a rectangle
 function colorRect(leftX, topY, width, height, drawColor){
   canvasContext.fillStyle = drawColor;
-  canvasContext.fillRect(leftX, topY, width, height); // (x-coor,y-coor,html width,html height)
+  canvasContext.fillRect(leftX, topY, width, height); // (x-coor,y-coor,width,height)
 }
